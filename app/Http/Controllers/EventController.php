e5b6a9cbe46ad052680bd828330a7e712247dc4e
neemias <neemiassantos217@gmail.com>
2023-05-07 00:15:04 -0300

    Deu erro

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,69 +5,76 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
-    public function index(){
+    
+    public function index() {
 
         $search = request('search');
 
-        if($search){
+        if($search) {
 
             $events = Event::where([
                 ['title', 'like', '%'.$search.'%']
             ])->get();
 
-        }else{
+        } else {
             $events = Event::all();
-        }
-
-        return view ('welcome',[ 'events' => $events,'search'=>$search]);
+        }        
+    
+        return view('welcome',['events' => $events, 'search' => $search]);
 
     }
-
 
     public function create() {
         return view('events.create');
     }
-    
-     public function store(Request $request) {
+
+    public function store(Request $request) {
 
         $event = new Event;
 
         $event->title = $request->title;
-        $event->date =$request->date;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
         $event->items = $request->items;
 
-        //Image Upload
-        if($request->hasFile('image')&& $request->file('image')->isValid()){
-            
-            $requestImage =$request->image;
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
 
             $extension = $requestImage->extension();
-            
-            $imageName = md5($requestImage->getClientOriginalName().strtotime("now")).".".$extension;
 
-            $requestImage->move(public_path('img/events'),$imageName);
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
 
-            $event->image =$imageName;
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
 
         }
+
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
-        return redirect('/')->with('msg','Evento criado com sucesso!');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
 
     }
 
     public function show($id) {
-        
+
         $event = Event::findOrFail($id);
 
-        return view('events.show',['event'=>$event]);
-    
-    }
-}
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        
+    }
+
+}
